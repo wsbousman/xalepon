f06e971e82699a5e4427d01f8dc3fa216d7b0fa6
wsbousman <85909621+wsbousman@users.noreply.github.com>
2023-06-28 17:39:54 -0500

    Dep
    Dep

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
         <img src="https://i.imgur.com/Ux5pj1Y.png" className="lbdk"/>
       </div>
       <div>
-        <p className="xalepon">branch</p>
+        <p className="xalepon"></p>
       </div>
     </div>
   );
