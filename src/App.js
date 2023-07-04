afd5a59a35f01521a687331725d90022e81cb808
wsbousman <85909621+wsbousman@users.noreply.github.com>
2023-07-04 01:16:48 -0500

    Sorted
    gh-pages branch manually merged with main. Builds locally. Ready to test via GitHub Pages. GitHub is an awful utility.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Gallery from './Components/Gallery/Index.js';
-import Header from './Components/Header/Index.js';
-import Nav from './Components/Nav/Index.js';
-import Process from './Components/Process/Index.js';
+import Gallery from './components/Gallery/Index.js';
+import Header from './components/Header/Index.js';
+import Nav from './components/Nav/Index.js';
+import Process from './components/Process/Index.js';
 import './App.css';
 
 class App extends Component {
