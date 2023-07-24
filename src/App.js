724c4eb670e20d2bafa7a4e78c9f2144bba736fa
wsbousman <85909621+wsbousman@users.noreply.github.com>
2023-07-24 03:45:20 -0500

    Div soup
    Images wrapped in img divs

--- src/App.js
+++ src/App.js
@@ -35,27 +35,53 @@ class App extends Component {
     <div className="img">
       <img src="https://i.imgur.com/n9omRMT.png" alt="Lout"/>
     </div>
+    <div className="img">
       <img src="https://i.imgur.com/DTv82Pb.png" alt="Akribos"/>
-      <p className="h3">I started off just kind of messing around, making handles for cheap pre-made knife blanks:</p>
+    </div>
+    <p className="h3">I started off just kind of messing around, making handles for cheap pre-made knife blanks:</p>
+    <div className="img">
       <img src="https://i.imgur.com/PKCpahl.png" alt="Buffalo"/>
+    </div>
+    <div className="img">
       <img src="https://i.imgur.com/OG0X8Wa.png" alt="Santoku"/>
-      <p className="h3">...And graduated to nicer materials, regrinding/profiling/finishing/sharpening blades, honing my craft:</p>
+    </div>
+    <p className="h3">...And graduated to nicer materials, regrinding/profiling/finishing/sharpening blades, honing my craft:</p>
+    <div className="img">
       <img src="https://i.imgur.com/FP4S1OI.png" alt="Furey"/>
+    </div>
+    <div className="img">
       <img src="https://i.imgur.com/OVw2LY0.png" alt="Ramsey"/>
-      <p className="h3">These days I do full stock removal knifemaking from premium steels, always with exotic but robust handle materials.</p>
+    </div>
+    <p className="h3">These days I do full stock removal knifemaking from premium steels, always with exotic but robust handle materials.</p>
+    <div className="img">
       <img src="https://i.imgur.com/PYQU5zD.png" alt="Little"/>
-      <p className="h3">Sometimes with fancy PVD/CVD coatings:</p>
+    </div>
+    <p className="h3">Sometimes with fancy PVD/CVD coatings:</p>
+    <div className="img">
       <img src="https://i.imgur.com/k7eaRFc.png" alt="Shorty"/>
-      <p className="h3">Or acid etching and flame anodizing titanium:</p>
+    </div>
+    <p className="h3">Or acid etching and flame anodizing titanium:</p>
+    <div className="img">
       <img src="https://i.imgur.com/4oUx89l.png" alt="Malocchio Handles"/>
+    </div>
+    <div className="img">
       <img src="https://i.imgur.com/2boFucy.png" alt="TiTiBar"/>
-      <p className="h3">Id guestimate each knife entailing around 50 hours of work.</p>
+    </div>
+    <p className="h3">Id guestimate each knife entailing around 50 hours of work.</p>
+    <div className="img">
       <img src="https://i.imgur.com/DEukKwJ.png" alt="Scrap"/>
-      <p className="h3">Which leaves me with a never-ending slew of unfinished projects:</p>
+    </div>
+    <p className="h3">Which leaves me with a never-ending slew of unfinished projects:</p>
+    <div className="img">
       <img src="https://i.imgur.com/wIZma1n.png" alt="Hubris"/>
+    </div>
+    <div className="img">
       <img src="https://i.imgur.com/HSqofTH.png" alt="Raw"/>
-      <p className="h3">So I do not accept commissions. But thanks for looking.</p>
+    </div>
+    <p className="h3">So I do not accept commissions. But thanks for looking.</p>
+    <div className="img">
       <img src="https://i.imgur.com/OXuycKm.png" alt="Bench"/>
+    </div>
     </div>
   );
 }
